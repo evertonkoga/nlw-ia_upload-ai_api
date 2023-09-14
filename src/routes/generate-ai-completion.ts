40f651dc92319d9d432dc6d708f9d686f340990c
everton.koga <everton.koga@tmov.com.br>
2023-09-14 11:47:44 -0300

    alterada api de 'completion' para retornar em stream com a lib 'ai'
    também foi criado/configurado o seed para o prisma

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { z } from 'zod';
+import { streamToResponse, OpenAIStream } from 'ai';
 import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 
@@ -8,11 +9,11 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
     app.post('/ai/complete', async (request, response) => {
         const bodyShema = z.object({
             videoId: z.string().uuid(),
-            template: z.string(),
+            prompt: z.string(),
             temperature: z.number().min(0).max(1).default(0.5),
         })
 
-        const { videoId, template, temperature } = bodyShema.parse(request.body);
+        const { videoId, prompt, temperature } = bodyShema.parse(request.body);
         const video = await prisma.video.findFirstOrThrow({
             where: {
                 id: videoId
@@ -21,16 +22,24 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
 
         if(!video.transcription) return response.status(400).send({ error: 'Video transcription was not generated yet.'});
 
-        const promptMessage = template.replace('{transcription}', video.transcription);
+        const promptMessage = prompt.replace('{transcription}', video.transcription);
 
         const responseCompletion = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo-16k',
             temperature,
             messages: [
                 { role: 'user', content: promptMessage },
-            ]
+            ],
+            stream: true,
         })
 
-        return responseCompletion;
+        const stream = OpenAIStream(responseCompletion);
+
+        streamToResponse(stream, response.raw, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            }
+        });
     })
 }
